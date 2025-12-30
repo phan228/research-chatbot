7d1a2ab86d57de363ac20d5b05e5f3c55417d79c
Ngoc Phan <ngocph228@gmail.com>
2025-12-30 16:39:51 -0500

    update main with system prompt & chat model

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import init_chat_model
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_agent
 from langchain.agents import create_agent, AgentExecutor
+import re
+import os
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -14,32 +17,16 @@ class ResearchResponse(BaseModel):
     references: list[str]
     tools_used: list[str]
 
-gpt = ChatOpenAI(model="gpt-5-mini", temperature=0)
+default_model = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
+#gpt = init_chat_model("gpt-4.1")
+gpt = ChatOpenAI(model=default_model, temperature=0)
 #claude = ChatAnthropic(model="claude-3-5-sonnet-20241022", temperature=0)
 
 # Parse the response into a ResearchResponse object to use
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-    ("system", 
-     """You are a research assistant that provides detailed summaries on various topics.
-     Answer the user query and use necessary tools to gather information.
-     Wrap the response in this formar and provide no other text\n{format_instructions}
-     """),
-     ("placeholder", "{chat_history}"),
-     ("human", "{user_input}"),
-     ("assistant", "{agent_scratchpad}")
-    ]
-).partial(format_instructions=parser.get_format_instructions())
-
-agent = create_agent(
-    model=gpt,
-    prompt=prompt,
-    tools=[]
+system_prompt = (
+    "You are a research assistant that provides detailed summaries on various topics. "
+    "Answer the user query and use necessary tools to gather information. "
+    f"Wrap the response in this format and provide no other text:\n{parser.get_format_instructions()}"
 )
-
-agent_executor = AgentExecutor(agent=agent, tools=[],verbose=True)
-raw_response = agent_executor.invoke({})
-
-#response = gpt.invoke("What is the capital of France?")
